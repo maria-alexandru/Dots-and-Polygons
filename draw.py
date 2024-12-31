c9bdf49fce83c5f314330524cefa54b0b8ab13c1
Maria Alexandru <mm.maria.alexandru@gmail.com>
2024-12-31 10:54:14 +0200

    still have errors
    Signed-off-by: Maria Alexandru <mm.maria.alexandru@gmail.com>

--- draw.py
+++ draw.py
@@ -1,5 +1,9 @@
 import ctypes
 import pygame
+import game_manager
+# import detect_surface
+from scipy.spatial import distance
+import math
 
 pygame.init()
 info = pygame.display.Info()
@@ -13,14 +17,15 @@ screen_width, screen_height = 1280, 720
 win = pygame.display.set_mode((screen_width, screen_height), pygame.RESIZABLE)
 pygame.display.set_caption("Dots and Polygons Menu")
 
-grid_size = 8
-cell_size = min(screen_height / (grid_size + 1), screen_width / (grid_size + 1))
+grid_size = 7
 
-rows = cols = grid_size
+rows = cols = grid_size - 1
 LINE_WIDTH = 8
 DOT_RADIUS = 8
 DOT_COLOR = (0, 0, 0)
 BACKGROUND_COLOR = (240, 240, 240)
+
+cell_size = min(screen_height / (grid_size + 1), screen_width / (grid_size + 1))
 
 screen = screen_width, screen_height
 padding_width = (screen_width - (grid_size + 1) * cell_size) / 2
@@ -30,74 +35,153 @@ lines = []
 selected_points = []
 connected_points = []
 
-def get_size():
+def get_size(cells):
     global screen_width, screen_height, info, padding_height, padding_width, cell_size
 
     # get the screen resolution (width and height)
     info = pygame.display.Info()
+    # update_points()
     screen_width = info.current_w
     screen_height = info.current_h
     cell_size = min(screen_height / (grid_size + 1), screen_width / (grid_size + 1))
     padding_width = (screen_width - (grid_size + 1) * cell_size) / 2
     padding_height = (screen_height - (grid_size + 1) * cell_size) / 2
 
+    for cell in cells:
+        cell.update_dim( cell_size, padding_width, padding_height)
+
+def update_points():
+    global selected_points
+    print("*************selected points: " + str(selected_points))
+    info = pygame.display.Info()
+    updated_points = [(math.floor(x * screen_width / info.current_w), math.floor(y * screen_height / info.current_h)) for x, y in selected_points]
+    print(updated_points)
+    selected_points = updated_points
+
+
 def draw_background():
     win.fill(BACKGROUND_COLOR)
 
-def draw_grid():
-    get_size()
+def draw_grid(cells):
+    get_size(cells)
     draw_background()
+    # pygame.display.flip()
 
-    for r in range(rows):
-        for c in range(cols):
-            pygame.draw.circle(win, DOT_COLOR, ((c + 1) * cell_size + padding_width, (r + 1) * cell_size + padding_height), DOT_RADIUS)
+    # for r in range(rows):
+    #     for c in range(cols):
+    #         pygame.draw.circle(win, DOT_COLOR, ((c + 1) * cell_size + padding_width, (r + 1) * cell_size + padding_height), DOT_RADIUS)
+    
+    print("////////////////////")
+    for index, cell in enumerate(cells):
+        pygame.draw.circle(win, DOT_COLOR, cell.rect.topleft, DOT_RADIUS)
+        pygame.draw.circle(win, DOT_COLOR, cell.rect.topright, DOT_RADIUS)
+        pygame.draw.circle(win, DOT_COLOR, cell.rect.bottomright, DOT_RADIUS)
+        pygame.draw.circle(win, DOT_COLOR, cell.rect.bottomleft, DOT_RADIUS)
+        # print("--------------")
+        # print("rect idx" + str(index)+ str(cell.rect))
+        # print("0 - 1 (nr 0)" + str(cell.dots[0]) + " " + str(cell.dots[1])+ " " + str(cell.sides[0]))
+        # print("1 - 2 (nr 1)" + str(cell.dots[1]) + " " + str(cell.dots[2])+ " " + str(cell.sides[1]))
+        # print("2 - 3 (nr 2)" + str(cell.dots[2]) + " " + str(cell.dots[3])+ " " + str(cell.sides[2]))
+        # print("3 - 0 (nr 3)" + str(cell.dots[3]) + " " + str(cell.dots[0])+ " " + str(cell.sides[3]))
+        # print("--------------")
+
+        detect_and_color_surface(cells, game_manager.GameManager().selected_mode)
+        for side in cell.sides:
+            index = cell.sides.index(side)
+            print(index)
+            if side == True:
+                draw_line(cell.edges[index][0],  cell.edges[index][1], (130, 208, 209), cells)
+
+        for index, dot in enumerate(cell.dots):
+            if dot == True:
+                draw_circle(cell.points[index][0], cell.points[index][1], (130, 109, 168))
+
+    print("////////////////////")
+
+    pygame.display.update()
 
 
-#verify if there is a dot between lines
+
+
+def detect_and_color_surface(cells, mode):
+    for cell in cells:
+        # detect complete square
+        if mode in ["Square", "Mix"] and cell.is_square_complete():
+            print("rect");
+            pygame.draw.rect(win, (0, 255, 0), cell.rect)
+            draw_line(cell.rect.topleft, cell.rect.topright, (130, 208, 209), cells)
+            draw_line(cell.rect.topright, cell.rect.bottomright, (130, 208, 209), cells)
+            draw_line(cell.rect.bottomright, cell.rect.bottomleft, (130, 208, 209), cells)
+            draw_line(cell.rect.bottomleft, cell.rect.topleft, (130, 208, 209), cells)
+            
+            pygame.draw.circle(win, (130, 109, 168), cell.rect.topright, DOT_RADIUS + 4)
+            pygame.draw.circle(win, (130, 109, 168), cell.rect.topleft, DOT_RADIUS + 4)
+            pygame.draw.circle(win, (130, 109, 168), cell.rect.bottomleft, DOT_RADIUS + 4)
+            pygame.draw.circle(win, (130, 109, 168), cell.rect.bottomright, DOT_RADIUS + 4)
+            
+            cell.winner = "Player"
+
+        if mode in ["Triangle", "Mix"] and cell.is_triangle_complete():
+
+            if cell.sides[0] and cell.sides[3] and cell.sides[4]:
+                triangle_points = [cell.rect.topleft, cell.rect.topright, cell.rect.bottomleft]
+                pygame.draw.polygon(win, (255, 0, 0), triangle_points)
+                draw_line(cell.rect.topleft, cell.rect.topright, (130, 208, 209), cells)
+                draw_line(cell.rect.topleft, cell.rect.bottomleft, (130, 208, 209), cells)
+                draw_line(cell.rect.topright, cell.rect.bottomleft, (130, 208, 209), cells)
+                
+                pygame.draw.circle(win, (130, 109, 168), cell.rect.topright, DOT_RADIUS + 4)
+                pygame.draw.circle(win, (130, 109, 168), cell.rect.topleft, DOT_RADIUS + 4)
+                pygame.draw.circle(win, (130, 109, 168), cell.rect.bottomleft, DOT_RADIUS + 4)
+
+            elif cell.sides[2] and cell.sides[1] and cell.sides[5]:
+                triangle_points = [cell.rect.topright, cell.rect.bottomright, cell.rect.bottomleft]
+                pygame.draw.polygon(win, (255, 0, 0), triangle_points)
+                draw_line(cell.rect.topright, cell.rect.bottomright, (130, 208, 209), cells)
+                draw_line(cell.rect.topright, cell.rect.bottomleft, (130, 208, 209), cells)
+                draw_line(cell.rect.bottomleft, cell.rect.bottomright, (130, 208, 209), cells)
+
+                pygame.draw.circle(win, (130, 109, 168), cell.rect.topright, DOT_RADIUS + 4)
+                pygame.draw.circle(win, (130, 109, 168), cell.rect.bottomleft, DOT_RADIUS + 4)
+                pygame.draw.circle(win, (130, 109, 168), cell.rect.bottomright, DOT_RADIUS + 4)
+
+            else:
+                continue
+
+            cell.winner = "Player"
+
+
+# verify if there is a dot between lines
 def is_point_inside_cell(point, cell):
     return cell.rect.collide_circle(point)
 
 # draw line between dots
 def draw_line(start, end, color, cells):
     global lines
+    pygame.draw.line(win, color, start, end, LINE_WIDTH)
+    pygame.draw.circle(win, (130, 109, 168), start, DOT_RADIUS + 4)
+    pygame.draw.circle(win, (130, 109, 168), end, DOT_RADIUS + 4)
 
-    # normalize points
-    line = tuple(sorted((start, end)))
 
-    if line not in lines:
-        pygame.draw.line(win, color, start, end, LINE_WIDTH)
-        lines.append(line)
+    # # normalize points
+    # line = tuple(sorted((start, end)))
 
-        if start not in connected_points:
-            connected_points.append(start)
-        if end not in connected_points:
-            connected_points.append(end)
+    # if line not in lines:
+    #     lines.append(line)
+
+        # if start not in connected_points:
+        #     connected_points.append(start)
+        # if end not in connected_points:
+        #     connected_points.append(end)
 
         
-        for cell in cells:
-            for edge_index, edge in enumerate(cell.edges):
-                if line == edge:
-                    cell.sides[edge_index] = True
-            # if line in cell.edges:
-            #     #horizontal
-            #     if start[1] == end[1]:
-            #         if start[0] < end[0]:
-            #             cell.sides[0] = True
-            #         else:
-            #             cell.sides[2] = True
-            #     elif start[0] == end[0]: #vertical
-            #         if start[1] < end[1]:
-            #             cell.sides[3] = True
-            #         else:
-            #             cell.sides[1] = True
-            #     else:
-            #         if start[0] < end[0]:
-            #             cell.sides[4] = True
-            #         elif start[0] > end[0]:
-            #             cell.sides[5] = True
+        # for cell in cells:
+        #     for edge_index, edge in enumerate(cell.edges):
+        #         if line == edge:
+        #             cell.sides[edge_index] = True
 
-        return True
-    return False
+    #     return True
+    # return False
 
 # check if two points are adjacent
 def is_adjacent(p1, p2):
@@ -105,13 +189,14 @@ def is_adjacent(p1, p2):
     x2, y2 = p2
 
     # horizontal adjacency
-    if abs(x1-x2) == cell_size and y1 == y2:
+    if abs(abs(x1-x2) - cell_size) <= 1 and abs(y1 - y2) <= 1:
         return True
 
-    if abs(y1 - y2) == cell_size and x1 == x2:
+    if abs(abs(y1 - y2) - cell_size) <= 1 and abs(x1 - x2) <= 1:
         return True
 
-    if abs(x1 - x2) == cell_size and abs(y1 - y2) == cell_size:
+    print(str(x1) + " " + str(x2) + " " + str(cell_size))
+    if abs(abs(x1 - x2) - cell_size) <= cell_size and abs(abs(y1 - y2) - cell_size) <= cell_size:
         return True
     return False
 
@@ -122,34 +207,84 @@ def is_connected(point):
             return True
     return False
 
-#select two dots and draw the line
-def select_point(r, c, pos, cells):
+# select two dots and draw the line
+def select_point(cell, pos):
     global selected_points
-    circle_center = ((c + 1) * cell_size + padding_width, (r + 1) * cell_size + padding_height)
-    if collide_circle(r, c, pos):
-        selected_points.append(circle_center)
+    # circle_center = (math.floor((cell.c + 1) * cell_size + padding_width), math.floor((cell.r + 1) * cell_size + padding_height))
+    collide, index = collide_circle(cell, pos)
+    if collide:
+        # selected_points.append(circle_center)
+        if cell.points[index] not in selected_points:
+            selected_points.append(cell.points[index])
+            print(selected_points)
 
-        #try to draw the line
-        if len(selected_points) == 2:
-            if is_adjacent(selected_points[0], selected_points[1]):    
-                draw_line(selected_points[0], selected_points[1], (130, 208, 209), cells)
+def try_draw_line(cells):
+    global selected_points
+    # try to draw the line
+    if len(selected_points) >= 2:
+        # print(selected_points[0])
+        # print(selected_points[1])
+        for point1 in selected_points:
+            for point2 in selected_points:
+                if is_adjacent(point1, point2) and point1 != point2:   
+                    line = tuple(sorted((point1, point2)))
+                    
+                    for cell in cells:
+                        for edge_index, edge in enumerate(cell.edges):
+                            edge = tuple(sorted((edge[0], edge[1])))
+                            # print("--------")
+                            # print(line)
+                            # print(edge)
+                            # print("-------")
+
+                            x1, y1 = line[0]
+                            x2, y2 = line[1]
+                            x3, y3 = edge[0]
+                            x4, y4 = edge[1]
+
+                            if math.sqrt((x3 - x1)**2 + (y3 - y1)**2) <= 2\
+                                and math.sqrt((x4 - x2)**2 + (y4 - y2)**2) <= 2:
+                                cell.sides[edge_index] = True
+                                # print("ff")
+                                print(edge_index) 
+                                print("adiacent")
+                                draw_line(point1, point2, (130, 208, 209), cells)
+                                selected_points = []
+                            # print("linie nu buna")
+                    
+                    # for point in selected_points:
+                    #     pygame.draw.circle(win, (130, 109, 168), point, DOT_RADIUS + 4)
                 
-                for point in selected_points:
-                    pygame.draw.circle(win, (130, 109, 168), point, DOT_RADIUS + 4)
-            #reset selection
+                    
+        if len(selected_points) >= 8:
+        #     #reset selection
             selected_points = []
 
 # draw colored dots when it is selected
-def draw_circle(r, c, color):
-    pygame.draw.circle(win, color, ((c + 1) * cell_size + padding_width, (r + 1) * cell_size + padding_height), DOT_RADIUS + 4)
+def draw_circle(x, y, color):
+    # pygame.draw.circle(win, color, (c * cell_size + padding_width, r * cell_size + padding_height), DOT_RADIUS + 4)
+    pygame.draw.circle(win, color, (x, y), DOT_RADIUS + 4)
 
-# check if mouse click is inside the dot
-def collide_circle(r, c, pos):
-    circle_center = ((c + 1) * cell_size + padding_width, (r + 1) * cell_size + padding_height)
-    circle_radius = DOT_RADIUS + 5
+# check if mouse click is inside the cell
+def collide_circle(cell, pos):
+    circle_radius = DOT_RADIUS + cell_size / 8
 
     px, py = pos
-    cx, cy = circle_center
+    cx, cy = cell.rect.topleft
     if (px - cx)**2 + (py - cy)**2 <= circle_radius**2:
-        return True
-    return False
+        return (True, 0)
+    
+    cx, cy = cell.rect.topright
+    if (px - cx)**2 + (py - cy)**2 <= circle_radius**2:
+        return (True, 1)
+    
+    cx, cy = cell.rect.bottomright
+    if (px - cx)**2 + (py - cy)**2 <= circle_radius**2:
+        return (True, 2)
+    
+    cx, cy = cell.rect.bottomleft
+    if (px - cx)**2 + (py - cy)**2 <= circle_radius**2:
+        return (True, 3)
+    
+    
+    return (False, -1)
--- game_manager.py
+++ game_manager.py
@@ -5,53 +5,87 @@ import detect_surface
 
 
 class GameManager:
+    _instance = None
+    
     def __init__(self):
         self.selected_mode = "Mix"
+        
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(GameManager, cls).__new__(cls)
+        return cls._instance
+
 
     def run(self):
-
-
         # add cells
         cells = []
+        nr = 0
         for c in range(draw.cols):
             for r in range(draw.rows):
+                nr = nr + 1
                 cell = cell_class.Cell(r, c, draw.cell_size, draw.padding_width, draw.padding_height)
                 cells.append(cell)
-
+        print("NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN" + str(nr))
 
         running = True
         pos = ()
-        draw.draw_grid()
+        draw.draw_grid(cells)
 
         while running:
+            # print(draw.selected_points)
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-                if event.type == pygame.MOUSEBUTTONDOWN:
+                elif event.type == pygame.VIDEORESIZE:
+                    draw.draw_grid(cells)
+                elif event.type == pygame.MOUSEBUTTONDOWN:
                     pos = event.pos
+                    inside = False
+                    # check if a dot was selected
                     for cell in cells:
-                        draw.select_point(cell.r, cell.c, pos, cells)
-                if event.type == pygame.VIDEORESIZE:
-                    draw.draw_grid()
+                        collide, index = draw.collide_circle(cell, pos)
+                        if pos and collide:
+                            inside = True
+                            # print(cell.rect)
+                            # print(pos)
+                            # print(str(index) + " " + str(cell.points[index][0]) + " " + str(cell.points[index][1]))
+                            draw.draw_circle(cell.points[index][0], cell.points[index][1], (130, 109, 168))
+                            cell.dots[index] = True
+                            # if cell.r * draw.grid_size + cell.c - 1 >= 0:
+                            #     cells[cell.r * draw.grid_size + cell.c - 1].dots[1] = True
+                            # if (cell.r - 1) * draw.grid_size + cell.c - 1 < draw.grid_size:
+                            #     cells[(cell.r - 1) * draw.grid_size + cell.c + 1].dots[1] = True
+                            # if cell.r * draw.grid_size + cell.c - 1 >= 0:
+                            #     cells[cell.r * draw.grid_size + cell.c - 1].dots[1] = True
+                            # if cell.r * draw.grid_size + cell.c - 1 >= 0:
+                            #     cells[cell.r * draw.grid_size + cell.c - 1].dots[1] = True
 
-            detect_surface.detect_and_color_surface(cells, self.selected_mode)
+                            draw.select_point(cell, pos)
+                    # if inside == False:
+                    #     draw.selected_points = []
+                    draw.try_draw_line(cells)
+                    pos = (-1, -1)
+
+                            # break
+                    draw.detect_and_color_surface(cells, self.selected_mode)
+
+
+
             
-            for line in draw.lines:
-                pygame.draw.line(draw.win, (130, 208, 209), line[0], line[1], draw.LINE_WIDTH)
+            # for line in draw.lines:
+            #     pygame.draw.line(draw.win, (130, 208, 209), line[0], line[1], draw.LINE_WIDTH)
 
-            for r in range(draw.rows):
-                for c in range(draw.cols):
-                    point = ((c + 1) * draw.cell_size + draw.padding_width, (r + 1) * draw.cell_size + draw.padding_height)
-                    if point in draw.connected_points:
-                        pygame.draw.circle(draw.win, (130, 109, 168), point, draw.DOT_RADIUS + 4)
-                    else:
-                        pygame.draw.circle(draw.win, draw.DOT_COLOR, point, draw.DOT_RADIUS)
+            # for r in range(draw.rows):
+            #     for c in range(draw.cols):
+            #         point = ((c + 1) * draw.cell_size + draw.padding_width, (r + 1) * draw.cell_size + draw.padding_height)
+                    # if point in draw.connected_points:
+                    #     pygame.draw.circle(draw.win, (130, 109, 168), point, draw.DOT_RADIUS + 4)
+                    # else:
+                        # pygame.draw.circle(draw.win, draw.DOT_COLOR, point, draw.DOT_RADIUS)
 
-            # check if a dot was selected
-            for cell in cells:
-                if pos and draw.collide_circle(cell.r, cell.c, pos):
-                    print(cell.rect)
-                    draw.draw_circle(cell.r, cell.c, (130, 109, 168))
+            
 
             pygame.display.update()
 
